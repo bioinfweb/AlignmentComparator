ae38b2166129a39fb58547cd938c7c84c3b8a332
BenStoever <stoever@bioinfweb.info>
2012-09-25 15:21:03 +0000

    Test classes adjusted to changes of package name from info.bioinfweb.alignmentcomparator.data to info.bioinfweb.alignmentcomparator.document.

--- test/src/info/bioinfweb/alignmentcomparator/document/io/results/ResultsReaderWriterTest.java
+++ test/src/info/bioinfweb/alignmentcomparator/document/io/results/ResultsReaderWriterTest.java
@@ -16,10 +16,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package info.bioinfweb.alignmentcomparator.data.io.results;
+package info.bioinfweb.alignmentcomparator.document.io.results;
 
 
-import info.bioinfweb.alignmentcomparator.data.SuperAlignmentSequenceView;
+import info.bioinfweb.alignmentcomparator.document.SuperAlignmentSequenceView;
 
 import org.junit.* ;
 
