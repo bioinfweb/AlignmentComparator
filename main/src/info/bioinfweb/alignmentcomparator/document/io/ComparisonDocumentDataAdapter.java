ba7804a475a39940962a223bdb7f01b1e6bb0b97
BenStoever <stoever@bioinfweb.info>
2016-07-14 09:58:03 +0000

    ComparisonDocumentDataAdapter now used in JPhyloIOWritingUtils.

--- main/src/info/bioinfweb/alignmentcomparator/document/io/ComparisonDocumentDataAdapter.java
+++ main/src/info/bioinfweb/alignmentcomparator/document/io/ComparisonDocumentDataAdapter.java
@@ -10,12 +10,7 @@ import info.bioinfweb.jphyloio.ReadWriteParameterMap;
 import info.bioinfweb.jphyloio.dataadapters.JPhyloIOEventReceiver;
 import info.bioinfweb.jphyloio.dataadapters.MatrixDataAdapter;
 import info.bioinfweb.jphyloio.dataadapters.implementations.EmptyDocumentDataAdapter;
-import info.bioinfweb.jphyloio.events.ConcreteJPhyloIOEvent;
-import info.bioinfweb.jphyloio.events.meta.LiteralContentSequenceType;
-import info.bioinfweb.jphyloio.events.meta.LiteralMetadataContentEvent;
-import info.bioinfweb.jphyloio.events.meta.LiteralMetadataEvent;
-import info.bioinfweb.jphyloio.events.meta.URIOrStringIdentifier;
-import info.bioinfweb.jphyloio.events.type.EventContentType;
+import info.bioinfweb.jphyloio.utils.JPhyloIOWritingUtils;
 
 
 
@@ -31,10 +26,8 @@ public class ComparisonDocumentDataAdapter extends EmptyDocumentDataAdapter impl
 
 	@Override
 	public void writeMetadata(ReadWriteParameterMap parameters, JPhyloIOEventReceiver receiver) throws IOException {
-		receiver.add(new LiteralMetadataEvent(DOCUMENT_TAG_ID, null, new URIOrStringIdentifier(null, PREDICATE_IS_COMPARISON), 
-				new URIOrStringIdentifier(null, W3CXSConstants.DATA_TYPE_BOOLEAN), LiteralContentSequenceType.SIMPLE));
-		receiver.add(new LiteralMetadataContentEvent(true, null));
-		receiver.add(ConcreteJPhyloIOEvent.createEndEvent(EventContentType.META_LITERAL));
+		JPhyloIOWritingUtils.writeSimpleLiteralMetadata(receiver, DOCUMENT_TAG_ID, null, PREDICATE_IS_COMPARISON, 
+				W3CXSConstants.DATA_TYPE_BOOLEAN, true);
 	}
 	// TODO Create OTU lists for alignments?
 	
