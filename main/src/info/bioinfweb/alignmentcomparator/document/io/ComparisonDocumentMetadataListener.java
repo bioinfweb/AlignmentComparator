150e0f9fa7ca33d4f0e9f8eff957ba509e5cdb3f
BenStoever <stoever@bioinfweb.info>
2016-07-14 18:27:53 +0000

    Initial versions of ComparisonDocumentReader and ComparisonDocumentMetadataListener added.

---
+++ main/src/info/bioinfweb/alignmentcomparator/document/io/ComparisonDocumentMetadataListener.java
@@ -0,0 +1,61 @@
+package info.bioinfweb.alignmentcomparator.document.io;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import info.bioinfweb.alignmentcomparator.Main;
+import info.bioinfweb.commons.bio.CharacterStateSetType;
+import info.bioinfweb.jphyloio.JPhyloIOEventReader;
+import info.bioinfweb.jphyloio.events.JPhyloIOEvent;
+import info.bioinfweb.jphyloio.events.meta.LiteralMetadataEvent;
+import info.bioinfweb.jphyloio.events.type.EventContentType;
+import info.bioinfweb.jphyloio.push.JPhyloIOEventListener;
+import info.bioinfweb.jphyloio.push.ParentEventInformation;
+
+
+
+public class ComparisonDocumentMetadataListener implements JPhyloIOEventListener, IOConstants {
+	private CharacterStateSetType tokenType = null;
+	private List<List<Integer>> unalignedIndicesList = new ArrayList<List<Integer>>();
+	
+	
+	public CharacterStateSetType getTokenType() {
+		return tokenType;
+	}
+
+
+	public List<List<Integer>> getUnalignedIndicesList() {
+		return unalignedIndicesList;
+	}
+
+
+	@Override
+	public void processEvent(JPhyloIOEventReader source, ParentEventInformation parentEvents, JPhyloIOEvent event) throws Exception {
+		if (event.getType().getContentType().equals(EventContentType.ALIGNMENT) && (tokenType == null)) {  // Alignment definitions start before a token type was specified.
+			throw new IOException("This document does not contain an " + Main.APPLICATION_NAME
+					+ " token type declaration. Maybe it is not an " + Main.APPLICATION_NAME + " document?");
+		}
+		else if (event.getType().getContentType().equals(EventContentType.META_LITERAL_CONTENT)) {
+			if (parentEvents.isParentSequence(EventContentType.META_LITERAL, EventContentType.DOCUMENT)) {
+				if (PREDICATE_TOKEN_TYPE.equals(parentEvents.getDirectParent().asLiteralMetadataEvent().getPredicate().getURI())) {
+					try {
+						tokenType = CharacterStateSetType.valueOf(event.asLiteralMetadataContentEvent().getObjectValue().toString().trim());  // The object value should be a string.
+					}
+					catch (NullPointerException | IllegalArgumentException e) {
+						throw new IOException("The specified " + Main.APPLICATION_NAME + " token type declaration \"" 
+								+ event.asLiteralMetadataContentEvent().getObjectValue() + "\" is invalid.", e);
+					}
+				}
+				
+			  //TODO Also check format.
+			}
+			else if (parentEvents.isParentSequence(EventContentType.META_LITERAL, EventContentType.ALIGNMENT)) {
+				//TODO Read superaligmment data
+			}
+		}
+	}
+}
---
+++ main/src/info/bioinfweb/alignmentcomparator/document/io/ComparisonDocumentReader.java
@@ -0,0 +1,39 @@
+package info.bioinfweb.alignmentcomparator.document.io;
+
+
+import info.bioinfweb.alignmentcomparator.document.Document;
+import info.bioinfweb.jphyloio.ReadWriteParameterMap;
+import info.bioinfweb.jphyloio.formats.nexml.NeXMLEventReader;
+import info.bioinfweb.jphyloio.push.EventForwarder;
+import info.bioinfweb.libralign.model.io.AlignmentModelEventReader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+
+
+public class ComparisonDocumentReader {
+	public void read(File file, Document alignments) throws Exception {
+		FileInputStream stream = new FileInputStream(file);
+		try {
+			read(stream, alignments);
+		}
+		finally {
+			stream.close();
+		}
+	}
+	
+	
+	public void read(InputStream stream, Document alignments) throws Exception {
+		EventForwarder forwarder = new EventForwarder(new NeXMLEventReader(stream, new ReadWriteParameterMap()));  //TODO Specify parameters?
+		AlignmentModelEventReader alignmentReader = new AlignmentModelEventReader();  //TODO Specify factory
+		forwarder.getListeners().add(alignmentReader);
+		//TODO Add listener for document metadata and superalignment metadata
+		//TODO Add data model listener for comments
+		
+		forwarder.readAll();
+		
+		//TODO Edit document according to collected information. 
+	}
+}
