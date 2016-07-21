3be98c8e0de6a58b7f6ad2c7ef0db364d947a983
BenStoever <stoever@bioinfweb.info>
2016-07-21 14:39:20 +0000

    Copyright information updated in test project.

--- test/src/info/bioinfweb/alignmentcomparator/document/io/results/FastaReaderTest.java
+++ test/src/info/bioinfweb/alignmentcomparator/document/io/results/FastaReaderTest.java
@@ -1,3 +1,21 @@
+/*
+ * AlignmentComparator - Compare and annotate two alternative multiple sequence alignments
+ * Copyright (C) 2014-2016  Ben StÃ¶ver
+ * <http://bioinfweb.info/Software>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package info.bioinfweb.alignmentcomparator.document.io.results;
 
 
@@ -45,7 +63,7 @@ public class FastaReaderTest {
 //								@Override
 //								public void parseHeader(String text, DNASequence sequence) {}
 //							},
-	            new DNASequenceCreator(new AlignmentAmbiguityNucleotideCompoundSet()));  //TODO Was würde DNASequenceCreator anders machen? 
+	            new DNASequenceCreator(new AlignmentAmbiguityNucleotideCompoundSet()));  //TODO Was wï¿½rde DNASequenceCreator anders machen? 
 	    
 			Map<String, DNASequence> map = fastaReader.process();
 	    output(map);
@@ -65,7 +83,7 @@ public class FastaReaderTest {
 	    	  		new FastaHeaderParserInterface<Sequence<NucleotideCompound>, NucleotideCompound>() {
 	    	  			public void parseHeader(String header, Sequence<NucleotideCompound> sequence) {}
 							},
-	            new DNASequenceCreator(new AlignmentAmbiguityNucleotideCompoundSet()));  //TODO Was würde DNASequenceCreator anders machen? 
+	            new DNASequenceCreator(new AlignmentAmbiguityNucleotideCompoundSet()));  //TODO Was wï¿½rde DNASequenceCreator anders machen? 
 	    
 			Map<String, Sequence<NucleotideCompound>> map = fastaReader.process();
 	    output(map);
