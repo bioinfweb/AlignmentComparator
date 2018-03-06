af8ac4cb5e1e2dc3bd338b52b1a6618671ed2646
BenStoever <stoever@bioinfweb.info>
2018-03-06 17:28:34 +0000

    PosHighlightOverlay debugged.

--- main/src/info/bioinfweb/alignmentcomparator/gui/PosHighlightOverlay.java
+++ main/src/info/bioinfweb/alignmentcomparator/gui/PosHighlightOverlay.java
@@ -32,20 +32,24 @@ import info.bioinfweb.libralign.alignmentarea.tokenpainter.ColorOverlay;
 public class PosHighlightOverlay implements ColorOverlay {
 	@Override
 	public Color getColor(AlignmentArea alignmentArea, String sequenceID, int columnIndex) {
+		// The focused area seems to be set after this method is called the first time (after the first click in an area). 
+		// Therefore the wrong areas would contain highlights then. This should not be a problem, since the first click can 
+		// only generate a selection of with 0, which does not lead to a highlighting.
+		
 		AlignmentArea focusedArea = alignmentArea.getContainer().getFocusedAlignmentArea();
 		
-		if ((focusedArea != null) && (focusedArea != alignmentArea)) {
+		if ((focusedArea != null) && (focusedArea != alignmentArea) && (focusedArea.getSelection().getWidth() > 0)) {
 			SuperalingedModelIndexTranslator otherTranslator = 
 					((SuperalignedModelDecorator)focusedArea.getAlignmentModel()).getIndexTranslator();
-			int start = otherTranslator.getUnalignedIndex(sequenceID, focusedArea.getSelection().getFirstColumn()).getAfterValidIndex();
-			int end = otherTranslator.getUnalignedIndex(sequenceID, focusedArea.getSelection().getLastColumn()).getBeforeValidIndex();
-			//TODO Check if an index was out of range
-			//TODO -1 anywhere?
+			int start = otherTranslator.getUnalignedIndex(sequenceID, focusedArea.getSelection().getFirstColumn()).getAfter();
+			int end = otherTranslator.getUnalignedIndex(sequenceID, focusedArea.getSelection().getLastColumn()).getBefore();
 			
-			SuperalingedModelIndexTranslator ownTranslator = 
-					((SuperalignedModelDecorator)alignmentArea.getAlignmentModel()).getIndexTranslator();
-			if (Math2.isBetween(columnIndex, ownTranslator.getAlignedIndex(sequenceID, start), ownTranslator.getAlignedIndex(sequenceID, end))) {
-				return AlignmentComparisonComponent.HIGHLIGHT_COLOR;
+			if ((start > 0) && (end > 0)) {  // There cannot be a respective position if after the start or before the end are out of range.
+				SuperalingedModelIndexTranslator ownTranslator = 
+						((SuperalignedModelDecorator)alignmentArea.getAlignmentModel()).getIndexTranslator();
+				if (Math2.isBetween(columnIndex, ownTranslator.getAlignedIndex(sequenceID, start), ownTranslator.getAlignedIndex(sequenceID, end))) {
+					return AlignmentComparisonComponent.HIGHLIGHT_COLOR;
+				}
 			}
 		}
 		return null;
